8f8f11b59fdc6cb0107bcf469514c92b4f503583
Guilherme Rodrigues <guilherme.souza.1277516@sga.pucminas.br>
2023-07-27 16:17:13 -0300

    Barplot of Crimes per Year in MG's Capital Belo Horizonte

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 violent_crimes_12_17 = pd.read_csv('C:/Users/xshee/OneDrive/Área de Trabalho/MyStuff/Projects/MG Data Analysis/CSV Files/Banco Crimes Violentos - Atualizado 2012 a 2017.csv',
                                        sep=';',
@@ -12,9 +14,16 @@ violent_crimes_12_17 = violent_crimes_12_17.rename(columns={'Mês ': 'Mês'})
 violent_crimes = pd.concat([violent_crimes_12_17, violent_crimes_18_23])
 
 violent_crimes = violent_crimes.rename(columns={'Registros': 'registers',
-                                                                    'Natureza': 'type',
-                                                                    'Município': 'city',
-                                                                    'Cod IBGE': 'ibge_code',
-                                                                    'Mês': 'month',
-                                                                    'Ano': 'year'})
+                                                'Natureza': 'type',
+                                                'Município': 'city',
+                                                'Cod IBGE': 'ibge_code',
+                                                'Mês': 'month',
+                                                'Ano': 'year'})
 
+bh = violent_crimes[violent_crimes.city == 'BELO HORIZONTE']
+bh = bh[['registers', 'year']].groupby(['year']).sum(numeric_only=True).reset_index()
+sns.barplot(data=bh, x='year', y='registers')
+plt.title('Violent Crimes x Year in the Capital of MG')
+plt.ylabel('Registers')
+plt.xlabel('Year')
+plt.show()
